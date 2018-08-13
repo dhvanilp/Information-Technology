ceaf5ada445041fe4ccec4e37293dceee3a1df50
DhvanilP <dhvanilhparikh@gmail.com>
2018-08-13 10:43:53 +0530

    PC added

---
+++ PC/lab1/14.c
@@ -0,0 +1,15 @@
+/*learn.c*/
+#include<stdio.h>
+#include<omp.h>
+int main()
+{
+    int i=10;
+    printf("Value before pragma i=%d\n",i);
+    #pragma omp parallel num_threads(4) private(i)
+    {
+        printf("Value after entering pragma i=%d tid=%d\n",i, omp_get_thread_num());
+        i=i+omp_get_thread_num(); //adds thread_id to i
+        printf("Value after changing value i=%d tid=%d\n",i, omp_get_thread_num());
+    }
+    printf("Value after having pragma i=%d tid=%d\n",i, omp_get_thread_num());
+}
